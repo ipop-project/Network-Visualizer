164faad878bd5b9d57ca0c2c4eba6e4f3fc0e9c8
avinashnatesan <avinashnatesan@gmail.com>
2017-10-03 14:12:05 -0400

    Cytoscape Checkpoint 1

--- CentralVisualizer/static/js/ipop_mainpage.js
+++ CentralVisualizer/static/js/ipop_mainpage.js
@@ -40,7 +40,7 @@ $('#refreshbtn').qtip({
 $fontSlider = '<br><label>&ensp;Node Label Sizing</label><br><input id="fontSlider" type="range" min="0.5" max="8.5" value="1.5" step:"0.5" onchange="changeNodeLabelSize(this.value)" />';
 $('#config').append($fontSlider);
 
-$zoomLimits = '<br><label>Layout Zoom Limits</label><br><label>minZoom</label>&emsp;&emsp;&emsp;&emsp;<label>maxZoom</label><select class="form-control" id="minzoom" onchange="changeLayoutZoomLimits(this.value,this.id)"><option>0.1</option><option>0.2</option><option>0.3</option><option>0.4</option><option>0.5</option><option>0.6</option><option>0.7</option></select><select class="form-control" id="maxzoom" onchange="changeLayoutZoomLimits(this.value,this.id)"><option>2</option><option>2.5</option><option>3</option><option>3.5</option><option>4</option><option>4.5</option><option>5</option></select>';
+$zoomLimits = '<br><label>Layout Zoom Limits</label><br><label>minZoom</label><select class="form-control" onchange="changeLayoutZoomLimits(this.value,this.id)"><option>0.1</option><option>0.2</option><option>0.3</option><option>0.4</option><option>0.5</option><option>0.6</option><option>0.7</option></select><br><label>maxZoom</label><select class="form-control" onchange="changeLayoutZoomLimits(this.value,this.id)"><option>2</option><option>2.5</option><option>3</option><option>3.5</option><option>4</option><option>4.5</option><option>5</option></select>';
 $('#config').append($zoomLimits);
 
 function changeNodeLabelSize(newValue){
