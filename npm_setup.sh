7376890d6a23a11eb30466360b6f264eaccd991e
avinashnatesan <avinashnatesan@gmail.com>
2018-01-17 16:38:48 -0500

    Merged Visualizer Service - Cytoscape UI

--- npm_setup.sh
+++ npm_setup.sh
@@ -5,4 +5,4 @@ npm init
 npm i webpack --save-dev
 npm i babel-core babel-loader babel-preset-es2015 babel-preset-react --save-dev
 npm i bootstrap cytoscape fontawesome jquery react react-bootstrap react-dom --save-dev
-npm run watch
+npm run watch &
