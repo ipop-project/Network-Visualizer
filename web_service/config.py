efbbda0fb9eb449f952d934a932dea31615d76db
Ashish Nargundkar <apnargundkar@gmail.com>
2018-11-08 17:12:43 -0500

    Rotating log files are now limited in size

--- web_service/config.py
+++ web_service/config.py
@@ -1,19 +1,20 @@
 CONTAINER_CONFIG = {
     "ip": "0.0.0.0",
     "port": 5000,
-    "debug": False, # WARNING! NEVER set debug = True in production!
+    "debug": False,  # WARNING! NEVER set debug = True in production!
     "template_folder": "UI/templates",
     "static_folder": "UI/static",
     "logging": {
         "console_level": "DEBUG",
         "rot_fh_level": "WARN",
         "rot_fh_count": 5,
+        "rot_fh_max_file_size": 2097152,  # in bytes
         "rot_fh_fname": "network_visualizer.log"
     }
 }
 
 COLLECTOR_CONFIG = {
-    "flush_duration": 30, # in seconds
+    "flush_duration": 30,  # in seconds
     "mongo": {
         "host": "localhost",
         "port": 27017,
