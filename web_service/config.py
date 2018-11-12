d6a6f3eb0c4e02afd2b2608313717abfeb8e4589
Ashish Nargundkar <apnargundkar@gmail.com>
2018-11-12 11:55:47 -0500

    No more writing to nohup.err

--- web_service/config.py
+++ web_service/config.py
@@ -6,9 +6,9 @@ CONTAINER_CONFIG = {
     "static_folder": "UI/static",
     "logging": {
         "console_level": "WARN",
-        "rot_fh_level": "INFO",
+        "rot_fh_level": "WARN",
         "rot_fh_count": 5,
-        "rot_fh_max_file_size": 2097152,  # in bytes
+        "rot_fh_max_file_size": 20971520,  # in bytes
         "rot_fh_fname": "network_visualizer.log"
     }
 }
