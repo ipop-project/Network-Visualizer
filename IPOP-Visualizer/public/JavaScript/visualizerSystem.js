9763798f7eac9f65c37a68fb074e22ffcf3d58f4
Ek-karin <gvdibomin0521@gmail.com>
2019-08-21 16:40:29 +0700

    Boom - commit

--- IPOP-Visualizer/public/JavaScript/visualizerSystem.js
+++ IPOP-Visualizer/public/JavaScript/visualizerSystem.js
@@ -3,7 +3,7 @@ var intervalNo = new Date().toISOString().split(".")[0];
 var serverIP = '34.209.33.166:5000';
 var allowOrigin = 'https://cors-anywhere.herokuapp.com/';  /* you need to allow origin to get data from outside server*/
 var url = allowOrigin + 'http://' + serverIP + '/IPOP/overlays?interval=' + intervalNo + '&current_state=True'
-
+var temp = '0'
 var showAllOverlays = function () {
    
     $.getJSON(url)
