769b09a01ef86159abc0fe56269b1c4ebd24240a
YootTanA <oat_boonpalit@hotmail.com>
2019-09-05 08:39:59 +0900

    fixed npm problem

--- IPOP-Visualizer/setup.sh
+++ IPOP-Visualizer/setup.sh
@@ -2,7 +2,8 @@
 
 wget -qO- https://deb.nodesource.com/setup_9.x | bash -
 
-sudo apt-get install -y nodejs && sudo apt install npm
+sudo apt-get update
+sudo apt-get install -y nodejs && sudo apt-get install npm
 
 
 npm init
