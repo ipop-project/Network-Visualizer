fbc3d4039db8434d84681a29894d92967deb8ab8
YootTanA <oat_boonpalit@hotmail.com>
2019-09-05 08:36:35 +0900

    Update setup.sh

--- IPOP-Visualizer/setup.sh
+++ IPOP-Visualizer/setup.sh
@@ -2,8 +2,7 @@
 
 wget -qO- https://deb.nodesource.com/setup_9.x | bash -
 
-sudo apt-get install -y nodejs
-sudo apt install npm
+sudo apt-get install -y nodejs && sudo apt install npm
 
 
 npm init
